ab05042c5f877fd8e9d2829526d7a46dc8642e89
Abel Souza <abelsouzacosta@gmail.com>
2021-04-05 22:04:50 -0300

    feat(category): validando dados da requisição
    Utilizando celebrate para validar os dados da requisição

--- src/routes/category.routes.js
+++ src/routes/category.routes.js
@@ -2,18 +2,39 @@ const router = require('express').Router();
 const authentication = require('../middlewares/auth');
 const CategoryController = require('../controllers/CategoryController');
 const AssociationController = require('../controllers/AssociationController');
+const { celebrate, Joi, Segments } = require('celebrate');
 
 router.use(authentication);
 
 router.get("/list", CategoryController.read);
 
-router.post("/create", CategoryController.create);
+router.post("/create", celebrate({
+  [Segments.BODY]: {
+    name: Joi.string().required()
+  }
+}), CategoryController.create);
 
-router.put("/update/:id", CategoryController.update);
+router.put("/update/:id", celebrate({
+  [Segments.BODY]: {
+    name: Joi.string()
+  },
 
-router.delete("/delete/:id", CategoryController.delete);
+  [Segments.PARAMS]: {
+    id: Joi.number().required()
+  }
+}), CategoryController.update);
 
-router.get("/details/:id", CategoryController.details);
+router.delete("/delete/:id", celebrate({
+  [Segments.PARAMS]: {
+    id: Joi.number().required()
+  }
+}), CategoryController.delete);
+
+router.get("/details/:id", celebrate({
+  [Segments.PARAMS]: {
+    id: Joi.number().required()
+  }
+}), CategoryController.details);
 
 router.post("/associate", AssociationController.associate_book_category);
 
