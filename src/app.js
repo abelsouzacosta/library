4ceb557033fa2c7ae3a0766ea81d5c5dd6c1bd01
Abel Souza <abelsouzacosta@gmail.com>
2021-04-05 21:33:27 -0300

    feat(author): adicionando validação dos dados na rota
    Validando os dados da requisição nas rotas garantindo que cada um deles siga o padrão corretamente

--- src/app.js
+++ src/app.js
@@ -3,11 +3,13 @@ const express = require('express');
 const app = express();
 
 const router = require('./routes/routes');
+const { errors } = require('celebrate');
 
 const db = require('./database/index');
 
 app.use(express.json());
 app.use(router);
+app.use(errors);
 
 try {
   db.authenticate();
--- src/routes/author.routes.js
+++ src/routes/author.routes.js
@@ -2,18 +2,45 @@ const router = require('express').Router();
 const authentication = require('../middlewares/auth');
 const AuthorController = require('../controllers/AuthorController');
 const AssociationController = require('../controllers/AssociationController');
+const { celebrate, Joi, Segments } = require('celebrate');
 
 router.use(authentication);
 
 router.get("/list", AuthorController.read);
 
-router.post("/create", AuthorController.create);
+router.post("/create", celebrate({
+  [Segments.BODY]: {
+    name: Joi.string().required(),
+    description: Joi.string().required(),
+    date_of_birth: Joi.date().required(),
+    date_of_death: Joi.date()
+  }
+}), AuthorController.create);
 
-router.put("/update/:id", AuthorController.update);
+router.put("/update/:id", celebrate({
+  [Segments.BODY]: {
+    name: Joi.string(),
+    description: Joi.string(),
+    date_of_birth: Joi.date(),
+    date_of_death: Joi.date()
+  },
 
-router.delete("/delete/:id", AuthorController.delete);
+  [Segments.PARAMS]: {
+    id: Joi.number().required()
+  }
+}), AuthorController.update);
 
-router.get("/details/:id", AuthorController.details);
+router.delete("/delete/:id", celebrate({
+  [Segments.PARAMS]: {
+    id: Joi.number().required()
+  }
+}), AuthorController.delete);
+
+router.get("/details/:id", celebrate({
+  [Segments.PARAMS]: {
+    id: Joi.number().required()
+  }
+}), AuthorController.details);
 
 router.post("/associate", AssociationController.associate_book_author);
 
